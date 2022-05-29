39193f157beec6611717578645633f67acb3c070
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-05-29 19:45:31 +0530

    ok where is my mind at

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ yaml_path=$2
 echo "[DEBUG] output_path=$output_path"
 echo "[DEBUG] yaml_path=$yaml_path"
 
-ino -ino-path $yaml_path -output $output
+ino -ino-path $yaml_path -output $output_path
 
 if [ $? -ne 0 ]; then exit $?; fi
 
-echo "::set-output name=output_file::$output"
+echo "::set-output name=output_file::$output_path"
