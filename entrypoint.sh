1d2b62624d2b974abac0da1a351e16ddadb5cc47
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-05-29 19:33:04 +0530

    maybe fix the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 output=${INPUT_output_path:-/tmp/index.md}
 yaml_path=$INPUT_ino_path
 
+echo "[DEBUG] output_path=$output_path"
+echo "[DEBUG] yaml_path=$yaml_path"
+
 ino -ino-path $yaml_path -output $output
+
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo "::set-output name=output_file::$output"
